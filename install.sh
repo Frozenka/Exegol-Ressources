e8b27b8584b2b0da1cbda54525e0d39f4b1768fd
Frozenk <newolf-hackworld@wanadoo.fr>
2024-02-28 14:08:28 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
 cat tmux.conf >> ~/.exegol/my-resources/setup/tmux/tmux.conf
 cat aliases >>  ~/.exegol/my-resources/setup/zsh/aliases
+cat load_user_setup.sh >>  ~/.exegol/my-resources/setup/load_user_setup.sh
+cat history >> ~/.exegol/my-resources/setup/zsh/history
