ae16d413f8cb119a74c6965abcb8fdecb35dc63a
Frozenk <newolf-hackworld@wanadoo.fr>
2024-02-28 14:29:09 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,7 @@ cat tmux.conf >> ~/.exegol/my-resources/setup/tmux/tmux.conf
 cat aliases >>  ~/.exegol/my-resources/setup/zsh/aliases
 cat load_user_setup.sh >>  ~/.exegol/my-resources/setup/load_user_setup.sh
 cat history >> ~/.exegol/my-resources/setup/zsh/history
+
+echo "Veuillez entrer votre clé API WPSCAN :"
+read api_key
+echo wpscan --api-token $api_key --url "http://$TARGET/" --no-banner --plugins-detection aggressive >> ~/.exegol/my-resources/setup/zsh/history
