ee867d58b81baa268f0767702f308cb27bbd3c68
Frozenk <newolf-hackworld@wanadoo.fr>
2024-03-15 14:31:40 +0100

    Update load_user_setup.sh

--- load_user_setup.sh
+++ load_user_setup.sh
@@ -7,3 +7,4 @@ pip3 install pyftpdlib
 wget https://raw.githubusercontent.com/insidetrust/statistically-likely-usernames/master/jsmith.txt -P /opt
 echo "PermitRootLogin prohibit-password" >> /etc/ssh/sshd_config
 echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
+git clone https://github.com/r3motecontrol/Ghostpack-CompiledBinaries.git /opt/windows
