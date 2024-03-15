21dc894f595b18370e417df95ab18bf44fcae0c3
Frozenk <newolf-hackworld@wanadoo.fr>
2024-03-15 09:34:17 +0100

    Update load_user_setup.sh

--- load_user_setup.sh
+++ load_user_setup.sh
@@ -1,6 +1,7 @@
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 python3 -m pip install --user uploadserver
 pip3 install wsgidav
+echo  "export TERM=xterm-256color" >> ~/.zshrc
 pip install cheroot
 pip3 install pyftpdlib
 wget https://raw.githubusercontent.com/insidetrust/statistically-likely-usernames/master/jsmith.txt -P /opt
