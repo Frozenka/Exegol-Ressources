ef698700f0b06d08b58004283a204abe0c341501
Frozenk <newolf-hackworld@wanadoo.fr>
2024-03-29 18:00:51 +0100

    Update load_user_setup.sh

--- load_user_setup.sh
+++ load_user_setup.sh
@@ -1,9 +1,12 @@
 git clone https://github.com/Frozenka/Exegol-Ressources.git
 cd Exegol-Ressources
 bash install.sh
-cd ..
-rm -rf Exegol-Ressources
+
 bash $HOME/.exegol/exegol-resources/setup.sh
 cat tmux.conf > ~/.exegol/my-resources/setup/tmux/tmux.conf
 cat aliases >  ~/.exegol/my-resources/setup/zsh/aliases
 cat history > ~/.exegol/my-resources/setup/zsh/history
+
+
+cd ..
+rm -rf Exegol-Ressources
