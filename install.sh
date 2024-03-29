284ca55acce0a7bf0d063770396f13e961dca978
Frozenk <newolf-hackworld@wanadoo.fr>
2024-03-29 17:36:47 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 cat tmux.conf > ~/.exegol/my-resources/setup/tmux/tmux.conf
 cat aliases >  ~/.exegol/my-resources/setup/zsh/aliases
-
 cat history > ~/.exegol/my-resources/setup/zsh/history
 
 echo wpscan --api-token $api_key --url "http://$TARGET/" --no-banner --plugins-detection aggressive >> ~/.exegol/my-resources/setup/zsh/history
