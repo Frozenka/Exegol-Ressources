540f887fe808d23ed22cff359e64cd178c2d766c
Frozenk <newolf-hackworld@wanadoo.fr>
2024-03-29 16:57:57 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,6 @@ cat aliases >>  ~/.exegol/my-resources/setup/zsh/aliases
 cat load_user_setup.sh >>  ~/.exegol/my-resources/setup/load_user_setup.sh
 cat history >> ~/.exegol/my-resources/setup/zsh/history
 
-echo "Veuillez entrer votre clé API WPSCAN :"
-read api_key
 echo wpscan --api-token $api_key --url "http://$TARGET/" --no-banner --plugins-detection aggressive >> ~/.exegol/my-resources/setup/zsh/history
 mv ./win/* $HOME/.exegol/exegol-resources/windows
 mv ./Linux* $HOME/.exegol/exegol-resources/linux
