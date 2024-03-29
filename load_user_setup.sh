dca4c08f629fb8389ffa98771bffdfa01547a37d
Frozenk <newolf-hackworld@wanadoo.fr>
2024-03-29 18:39:42 +0100

    Update load_user_setup.sh

--- load_user_setup.sh
+++ load_user_setup.sh
@@ -1,10 +1,1 @@
-
-
-/bin/bash ./install.sh
-
-/bin/bash /$HOME/.exegol/exegol-resources/setup.sh
-
-
-cd ..
-rm -rf Exegol-Ressources
-/bin/bash /opt/setup.sh
+ wget https://raw.githubusercontent.com/Frozenka/Exegol-Ressources/main/install.sh || bash install.sh
