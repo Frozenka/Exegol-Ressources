f43463de7dfc51fff2c3582e2de04c8509ed6c74
Frozenk <newolf-hackworld@wanadoo.fr>
2024-04-28 18:10:13 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -42,6 +42,8 @@ cd /opt/resources/linux https://raw.githubusercontent.com/Frozenka/linfast/main/
 #Modification visuel uniquement :
 sed -i 's/"#"/"%{$fg[green]%}➜ %{$reset_color%}"/g' /opt/.exegol_shells_rc
 sed -i 's/"%m"/"${HOSTNAME#exegol-}"/g' /opt/.exegol_shells_rc
+sed -i 's/(%Z)//g' /opt/.exegol_shells_rc
+
 
 ${HOSTNAME#exegol-}
 
