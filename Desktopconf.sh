0e0cf5f126aef64e24d236a517c31bb6d0a3fd05
Frozenk <newolf-hackworld@wanadoo.fr>
2024-09-01 13:00:27 +0200

    Update Desktopconf.sh

--- Desktopconf.sh
+++ Desktopconf.sh
@@ -1,31 +1,18 @@
 #!/bin/bash
-
-# URL de l'image à télécharger
 IMAGE_URL="https://raw.githubusercontent.com/xct/kali-clean/main/.wallpaper/23.jpg"
-
-# Dossier de destination pour l'image
 IMAGE_DIR="/root/Downloads"
 IMAGE_PATH="$IMAGE_DIR/23.jpg"
-
-# Créer le dossier de destination si nécessaire
 mkdir -p "$IMAGE_DIR"
-
-# Télécharger l'image silencieusement
 curl -s -o "$IMAGE_PATH" "$IMAGE_URL"
-
-# Chemin vers le fichier de configuration de Terminator pour root
 TERMINATOR_CONFIG="/root/.config/terminator/config"
 
-# Créer le fichier de configuration de Terminator s'il n'existe pas
 if [ ! -f "$TERMINATOR_CONFIG" ]; then
     mkdir -p "$(dirname "$TERMINATOR_CONFIG")"
     touch "$TERMINATOR_CONFIG"
 fi
 
-# Ajouter ou remplacer les paramètres nécessaires dans le fichier de configuration
 cat <<EOF > "$TERMINATOR_CONFIG"
 [global_config]
-
 [profiles]
   [[default]]
   background_image = $IMAGE_PATH
