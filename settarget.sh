44e3acb74066b5a9fdc1b64a0a4eccf6bdeaacc9
Frozenk <frozenk@frozenk.fr>
2025-02-24 16:20:45 +0100

    Update settarget.sh

--- settarget.sh
+++ settarget.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-#Petit tool pour définir la cible avec TARGET
-
 # Vérifie si l'argument est passé
 if [ -z "$1" ]; then
   echo "Vous devez spécifier une cible !"
   exit 1
 fi
 
-# Remplacer l'ancienne valeur de TARGET par la nouvelle dans le fichier profile.sh
-sudo sed -i "/^export TARGET=/c\export TARGET='$1'" /opt/tools/Exegol-history/profile.sh
+# Supprimer toutes les lignes existantes contenant 'export TARGET'
+sudo sed -i '/^export TARGET=/d' /opt/tools/Exegol-history/profile.sh
+
+# Ajouter la nouvelle ligne export TARGET='$1' dans le fichier profile.sh
+echo "export TARGET='$1'" | sudo tee -a /opt/tools/Exegol-history/profile.sh > /dev/null
 
 # Affiche un message de confirmation
 echo "TARGET set to $1"
+
+# Appliquer immédiatement la modification dans l'environnement actuel
+export TARGET="$1"
